c663d322179963ba3993c00db5843d1c119b87cd
Asraelite <markusscully@gmail.com>
2015-02-14 12:26:20 +0000

    Add text rendering, opacity and resetting

--- pallet.js
+++ pallet.js
@@ -1,4 +1,6 @@
 function Pallet(canvas, options) {
+	var self = this;
+	
 	options = options || {};
 	
 	if(typeof canvas == 'string') {
@@ -15,13 +17,18 @@ function Pallet(canvas, options) {
 	this.canvas = canvas;
 	this.context = context;
 	
+	context.webkitImageSmoothingEnabled = options.imageSmoothing || true;
+	context.mozImageSmoothingEnabled = options.imageSmoothing || true;
+	
+	context.save();
+	
 	// Render single colour unstroked circle.
 	this.circle = function(color, x, y, radius) {
 		context.fillStyle = color;
 		context.beginPath();
 		context.arc(x, y, radius, 0, Math.PI * 2, false);
      	context.fill();
-	}
+	};
 	
 	// Clear canvas.
 	this.clear = function(x, y, w, h) {
@@ -38,12 +45,30 @@ function Pallet(canvas, options) {
 		
 		canvas.width = +style.width.replace('px', '');
 		canvas.height = +style.height.replace('px', '');
+		
+		context.save();
+	};
+	
+	this.opacity = function(alpha) {
+		alpha = alpha || 1;
+		context.globalAlpha = alpha;
 	};
 	
 	// Render single colour unstroked rectangle.
 	this.rect = function(color, x, y, w, h) {
 		context.fillStyle = color;
 		context.fillRect(x, y, w, h);
+	};
+	
+	// Restore canvas to original state.
+	this.reset = function() {
+		context.restore();
+		context.save();
+	};
+	
+	// Render rect with equal sides.
+	this.square = function(color, x, y, l) {
+		self.rect(color, x, y, l, l);
 	};
 	
 	// Render text.
