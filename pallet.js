2237685d0542dde939127e9c3cb3ac6a8d670766
Asraelite <markusscully@gmail.com>
2016-03-21 15:47:49 +0000

    Add fill function

--- pallet.js
+++ pallet.js
@@ -1,8 +1,8 @@
 function Pallet(canvas, options) {
 	var self = this;
-	
+
 	options = options || {};
-	
+
 	if(typeof canvas == 'string') {
 		canvas = document.getElementById(canvas.replace('#', ''));
 	} else if(!canvas) {
@@ -11,17 +11,17 @@ function Pallet(canvas, options) {
 	} else if(canvas.tagName != 'CANVAS') {
 		return false;
 	}
-	
+
 	var context = canvas.getContext('2d');
-	
+
 	this.canvas = canvas;
 	this.context = context;
-	
+
 	context.webkitImageSmoothingEnabled = options.imageSmoothing || true;
 	context.mozImageSmoothingEnabled = options.imageSmoothing || true;
-	
+
 	context.save();
-	
+
 	// Render single colour unstroked circle.
 	this.circle = function(color, x, y, radius) {
 		context.fillStyle = color;
@@ -29,58 +29,64 @@ function Pallet(canvas, options) {
 		context.arc(x, y, radius, 0, Math.PI * 2, false);
      	context.fill();
 	};
-	
+
 	// Clear canvas.
-	this.clear = function(x, y, w, h) {
-		if(!h) {
+	this.clear = function(x, y, w, h, color) {
+		if (!h) {
 			context.clearRect(0, 0, canvas.width, canvas.height);
 		} else {
 			context.clearRect(x, y, w, h);
 		}
 	};
-	
+
+	// Fill screen with color.
+	this.fill = function(color) {
+		context.fillStyle = color;
+		context.fillRect(0, 0, canvas.width, canvas.height);
+	}
+
 	// Resize canvas to window.
 	this.fillScreen = function() {
 		canvas.width = window.innerWidth;
 		canvas.height = window.innerHeight;
 		canvas.style.width = canvas.width + 'px';
 		canvas.style.height = canvas.height + 'px';
-		
+
 		self.normalizeSize();
 	};
-	
+
 	// Set canvas size to canvas element size.
 	this.normalizeSize = function() {
 		var style = window.getComputedStyle(canvas, null);
-		
+
 		canvas.width = +style.width.replace('px', '');
 		canvas.height = +style.height.replace('px', '');
-		
+
 		context.save();
 	};
-	
+
 	this.opacity = function(alpha) {
 		alpha = alpha || 1;
 		context.globalAlpha = alpha;
 	};
-	
+
 	// Render single colour unstroked rectangle.
 	this.rect = function(color, x, y, w, h) {
 		context.fillStyle = color;
 		context.fillRect(x, y, w, h);
 	};
-	
+
 	// Restore canvas to original state.
 	this.reset = function() {
 		context.restore();
 		context.save();
 	};
-	
+
 	// Render rect with equal sides.
 	this.square = function(color, x, y, l) {
 		self.rect(color, x, y, l, l);
 	};
-	
+
 	// Render text.
 	this.text = function(string, x, y, color, font, size, align, baseline) {
 		context.fillStyle = color || '#fff';
@@ -91,4 +97,3 @@ function Pallet(canvas, options) {
 		context.fillText(string, x, y);
 	};
 }
-
